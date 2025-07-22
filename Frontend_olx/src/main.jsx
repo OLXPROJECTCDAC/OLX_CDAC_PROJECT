29208615177d4812f2830c8ffc4a9af8dcb5ce71
Shubham <shubhamjadhav0806@gmail.com>
2025-07-22 15:10:17 +0530

    Created file src/components/Navbar.jsx

--- Frontend_olx/src/main.jsx
+++ Frontend_olx/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import HomePage from './pages/HomePage'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
