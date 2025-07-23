57fc0a28709f50835aa3ad05887ac6db8d42b7d3
Vaishanav Bhosale <vaishanavb1729@gmail.com>
2025-07-23 18:27:49 +0530

    AdminPanel component is created

--- Frontend_olx/src/pages/AdminPanel.jsx
+++ Frontend_olx/src/pages/AdminPanel.jsx
@@ -2,7 +2,7 @@
 
 import { chakra } from "@chakra-ui/react";
 import SlideBar from "../components/SideBar";
-import { useNavigate } from "react-router-dom";
+
 const AdminPanel = ()=>{
 
     return (
