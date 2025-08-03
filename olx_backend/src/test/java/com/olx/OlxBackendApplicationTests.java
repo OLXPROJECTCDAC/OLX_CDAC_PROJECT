5aaad53502f5b2f1c23828607b873ec815983649
Vaishanav_Bhosale <vaishanavb1729@gmail.com>
2025-08-03 20:23:26 +0530

    Update OlxBackendApplicationTests.java

--- olx_backend/src/test/java/com/olx/OlxBackendApplicationTests.java
+++ olx_backend/src/test/java/com/olx/OlxBackendApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")  // 👈 This tells Spring to use application-test.properties
+
 
 class OlxBackendApplicationTests {
 
