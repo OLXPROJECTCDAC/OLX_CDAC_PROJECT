9955ab2aa4085e837b9fddb46d40ee442b48c0ea
Shivraj <shivrajdeshmukh19@gmail.com>
2025-08-03 10:59:06 +0530

    Added wishlist, product photos and report entities, modified user and product entity

---
+++ olx_backend/src/main/java/com/olx/entity/WishlistEntity.java
@@ -0,0 +1,32 @@
+package com.olx.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "wishlist", uniqueConstraints = {
+	    @UniqueConstraint(columnNames = { "user_id", "product_id" })
+	})
+@Getter
+@Setter
+public class WishlistEntity extends BaseEntity {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", nullable = false)
+	private ProductEntity product;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private UserEntity user;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
+    
+}
